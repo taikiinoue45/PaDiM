d5672bf95e691394ddea19df690d58d274ca050d
TaikiInoue <taikiinoue45@gmail.com>
2021-04-21 07:05:09 +0000

    Fix dir structure

---
+++ run.py
@@ -0,0 +1,36 @@
+import logging
+import os
+import sys
+from pathlib import Path
+
+import hydra
+import mlflow
+from omegaconf import DictConfig
+
+from padim.runner import Runner
+
+
+log = logging.getLogger(__name__)
+
+config_path = str(Path(sys.argv[1]).parent)
+config_name = str(Path(sys.argv[1]).stem)
+sys.argv.pop(1)
+
+
+@hydra.main(config_path=config_path, config_name=config_name)
+def main(cfg: DictConfig) -> None:
+
+    # mlflow.set_tracking_uri(cfg.params.tracking_uri)
+    # mlflow.set_experiment(cfg.params.experiment_name)
+    # mlflow.start_run(run_name=cfg.params.run_name)
+    # mlflow.log_params(cfg.params)
+    # mlflow.log_param("cwd", os.getcwd())
+
+    runner = Runner(cfg)
+    runner.run()
+
+    mlflow.log_artifacts(".hydra", "hydra")
+
+
+if __name__ == "__main__":
+    main()
