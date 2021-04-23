481fb69f7a8b834e3188c49afb6ad3df7751c3b3
taikiinoue45 <taikiinoue45@gmail.com>
2021-04-23 11:57:25 +0900

    Add test_fixed_files

--- tests/test_coverage.py
+++ tests/test_coverage.py
@@ -1,10 +1,9 @@
 from omegaconf import OmegaConf
-from py._path.local import LocalPath
 
 from padim.runner import Runner
 
 
-def test_run(tmpdir: LocalPath) -> None:
+def test_run() -> None:
 
     cfg = OmegaConf.load("./config.yaml")
     runner = Runner(cfg)
