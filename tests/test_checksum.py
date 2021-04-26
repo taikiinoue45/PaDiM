6ebf1ee159b09991e0a6aabfa45889ee3df6cb1f
taikiinoue45 <taikiinoue45@gmail.com>
2021-04-26 15:44:17 +0900

    Check github actions behavior

--- tests/test_checksum.py
+++ tests/test_checksum.py
@@ -17,7 +17,7 @@ def test_checksum() -> None:
         "a7f3a19bf4f9bc916cae1fe88f7f4201603ce984c46db487ec9bb4e8449248dc",
         "981bd32ad4febe14a44c1c7a0a00245c747373f39d6e22884f80f018bebc68c8",
         "e18032c3c704dc1a391d72cf8bded1c98b0dffcc80d00e84f5a78daf53e4cc0f",
-        "1aea0b7a9ed1684334746ab70fd543fd33485c8ac27f265afbd59e2d09410639",
+        "554fcb0f83a81acee292a1c160bc6e6e7a8f174f16cbce0f992bb574a0dfddb5",
         "8b3737677c06afd488972bbabc16cd56362563cc748ade3ec0809b24ce54a59c",
     ]
 
