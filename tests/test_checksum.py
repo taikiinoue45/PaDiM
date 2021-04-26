eb10288072f35582b8ea129a0b822a4b3b0ca7ea
taikiinoue45 <taikiinoue45@gmail.com>
2021-04-26 12:05:01 +0900

    Fix clean up step

--- tests/test_checksum.py
+++ tests/test_checksum.py
@@ -17,8 +17,8 @@ def test_checksum() -> None:
         "a7f3a19bf4f9bc916cae1fe88f7f4201603ce984c46db487ec9bb4e8449248dc",
         "981bd32ad4febe14a44c1c7a0a00245c747373f39d6e22884f80f018bebc68c8",
         "e18032c3c704dc1a391d72cf8bded1c98b0dffcc80d00e84f5a78daf53e4cc0f",
-        "32a02a431f0c9970d9a80014fd76ccf41abb94f78f22ad3566befcd6e2ccc3f3",
-        "a6d40b05de024a78be5bd5936bf19bbaa96b725c41c7cfe2f34d3cf2af00fa0e",
+        "1aea0b7a9ed1684334746ab70fd543fd33485c8ac27f265afbd59e2d09410639",
+        "8b3737677c06afd488972bbabc16cd56362563cc748ade3ec0809b24ce54a59c",
     ]
 
     for file_path, correct_checksum in zip(file_path_list, checksum_list):
