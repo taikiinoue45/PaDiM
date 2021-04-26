6af44a04df0ad9267bce69185dbb05f10206b9f7
taikiinoue45 <taikiinoue45@gmail.com>
2021-04-26 11:44:05 +0900

    Fix clean up step

--- tests/test_checksum.py
+++ tests/test_checksum.py
@@ -17,7 +17,7 @@ def test_checksum() -> None:
         "a7f3a19bf4f9bc916cae1fe88f7f4201603ce984c46db487ec9bb4e8449248dc",
         "981bd32ad4febe14a44c1c7a0a00245c747373f39d6e22884f80f018bebc68c8",
         "e18032c3c704dc1a391d72cf8bded1c98b0dffcc80d00e84f5a78daf53e4cc0f",
-        "90c32add3f2200a217b6b080124ab0b23f4beaf807c1ed0e23e7be6dea91c828",
+        "32a02a431f0c9970d9a80014fd76ccf41abb94f78f22ad3566befcd6e2ccc3f3",
         "a6d40b05de024a78be5bd5936bf19bbaa96b725c41c7cfe2f34d3cf2af00fa0e",
     ]
 
