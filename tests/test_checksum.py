db93bdb84dec4649f30e0a86b52973e922e4a6fb
taikiinoue45 <taikiinoue45@gmail.com>
2021-04-26 17:31:19 +0900

    Test github actions

--- tests/test_checksum.py
+++ tests/test_checksum.py
@@ -17,8 +17,8 @@ def test_checksum() -> None:
         "a7f3a19bf4f9bc916cae1fe88f7f4201603ce984c46db487ec9bb4e8449248dc",
         "981bd32ad4febe14a44c1c7a0a00245c747373f39d6e22884f80f018bebc68c8",
         "e18032c3c704dc1a391d72cf8bded1c98b0dffcc80d00e84f5a78daf53e4cc0f",
-        "2ef1f792352e297f20717cb6028322a026c0c6211d40b91a2830b95806151c08",
-        "8b3737677c06afd488972bbabc16cd56362563cc748ade3ec0809b24ce54a59c",
+        "9bc9ff16c91f34ccfb5fbf69cdb8dfdc3305433088fdb89f39f0f74760849794",
+        "3c2a8acd4383bfc5e44009d3b539cb52566ecc648ccb459f2d7536b9f42f6e8d",
     ]
 
     for file_path, correct_checksum in zip(file_path_list, checksum_list):
